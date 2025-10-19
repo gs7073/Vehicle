5cecaf536b277c4073d4e6199b58944a4c0f1a05
Cyber_Mamriot <gs7073@bs.amalnet.k12.il>
2025-10-19 16:23:08 +0300

    Car.java

--- app/src/main/java/com/example/vehicle/Q2/Car.java
+++ app/src/main/java/com/example/vehicle/Q2/Car.java
@@ -1,4 +1,27 @@
 package com.example.vehicle.Q2;
 
-public class Car {
+class Car extends Vehicle
+{
+    Car(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    public String start()
+    {
+        return "engine is on";
+    }
+
+    @Override
+    public String accelerate()
+    {
+        return "car is speeding up";
+    }
+
+    @Override
+    public String stop()
+    {
+        return "car stopped";
+    }
 }
